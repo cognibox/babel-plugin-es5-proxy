5ec041ad471502b4d87a0948e84b89eb80ba9ec9
decaruju <julien.decarufel@gmail.com>
2019-07-03 14:51:42 -0400

    interface, use the JS interface

--- runtime.js
+++ runtime.js
@@ -1,30 +1,34 @@
-function defaultGet(property) {
-  return this[property]; // eslint-disable-line no-invalid-this
+function defaultGet(object, property) {
+  return object[property];
 }
 
-function defaultSet(property, value) {
-  this[property] = value; // eslint-disable-line no-invalid-this
+function defaultSet(object, property, value) {
+  object[property] = value;
   return value;
 }
 
 function globalGetter(object, propertyName) { // eslint-disable-line no-unused-vars
-  return (object instanceof Proxy) ? object.get(propertyName) : object[propertyName];
+  return (isProxy(object)) ? object.get(object.target, propertyName) : object[propertyName];
 }
 
 function globalSetter(object, propertyName, value) { // eslint-disable-line no-unused-vars
-  if (object instanceof Proxy) {
-    return object.set(propertyName, value);
+  if (isProxy(object)) {
+    return object.set(object.target, propertyName, value);
   }
   object[propertyName] = value;
   return value;
 }
 
+function isProxy(object) {
+  return object.constructor.name === 'Proxy';
+}
+
 function objectTarget(object) { // eslint-disable-line no-unused-vars
-  return (object instanceof Proxy) ? object.target : object;
+  return (isProxy(object)) ? object.target : object;
 }
 
 function Proxy(target, handlers = {}) { // eslint-disable-line no-unused-vars
   this.target = target;
-  this.get = (handlers.get || defaultGet).bind(this.target);
-  this.set = (handlers.set || defaultSet).bind(this.target);
+  this.get = handlers.get || defaultGet;
+  this.set = handlers.set || defaultSet;
 }
